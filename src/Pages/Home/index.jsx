8c71668cdc56476787acea81823c50096567d72c
Andre <leconaandre51@gmail.com>
2024-08-15 21:43:38 -0400

    Implementación inicial del filtro de productos mediante el input text

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,6 +8,28 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 function Home() {
 
     const context = useContext(ShoppingCartContext)
+
+    const renderView = ()=>{
+      if(context.searchByTitle?.length > 0){        
+        if (context.filteredItems?.length > 0 ) {
+          return(
+            context.filteredItems?.map(item => ( 
+              <Card key={item.id} data={item}w/>
+            )) 
+          )
+        }else{
+          return(
+            <div>We don t have anything :P </div>
+          )
+        }
+      }else{
+        return(
+          context.items?.map(item => ( 
+            <Card key={item.id} data={item}/>
+          ))        
+        )        
+      }
+    }
     
    
     return (
@@ -19,19 +41,17 @@ function Home() {
           </div>
           <div className='flex items-center justify-center gap-6'>
           <input 
-          className='border-b-2 border-black p-3 mb-3  w-96 rounded-lg focus:outline-none'
-          placeholder='Search a Product' 
           type="text" 
-          onChange={(event) => context.searchByTitle(event.target.value)}
+          placeholder='Search a Product' 
+          className='border-b-2 border-black p-3 mb-3  w-96 rounded-lg focus:outline-none'
+          onChange={(event) => context.setSearchByTitle(event.target.value)}
           />
     
           <MagnifyingGlassIcon className='w-6 h-6 text-black' />
           </div>
           <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
           {
-            context.items?.map(item => ( 
-              <Card key={item.id} data={item}w/>
-            ))
+            renderView()
           }
           </div>
           <ProductDetail />
